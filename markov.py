65c0beda9854be6cfce7c7cc5df7eff058c0003f
Hackbright Student <info@hackbrightacademy.com>
2015-10-01 18:06:05 -0700

    Started on smart Markov generator

--- markov.py
+++ markov.py
@@ -1,5 +1,6 @@
 import sys
 from random import choice
+from string import punctuation
 
 
 class SimpleMarkovGenerator(object):
@@ -10,11 +11,13 @@ class SimpleMarkovGenerator(object):
     def make_chains(self):
         """Given a list of files, make chains from them."""
         """Takes input text as string; stores chains."""
-
+        #Make a dictionary and split a text file into a list of words
         chains = {}
-
         words = self.corpus.split()
 
+        #MAKE THE DICTIONARY
+        ##ake a key/value pair, check if it's in the dictionary, and if not, add it.
+        ##If it's in there, append the value to the value list stored at the key.
         for i in range(len(words) - 2):
             key = (words[i], words[i + 1])
             value = words[i + 2]
@@ -24,23 +27,37 @@ class SimpleMarkovGenerator(object):
 
             chains[key].append(value)
 
-        key = choice((chains).keys())
-        words = [key[0], key[1]]
-        while key in chains:
-            # Keep looping until we have a key that isn't in the chains
-            # (which would mean it was the end of our original text)
-            #
-            # Note that for long texts (like a full book), this might mean
-            # it would run for a very long time.
+        ##MAKE THE MARKOV CHAIN
+        #Pick a random key from the dictionary
+        key = choice(chains.keys())
+        #Make sure it starts with an upper case letter before proceeding.
+        while key[0][0].isupper() == False:
+            key = choice(chains.keys())
 
+        #Split the key into two words
+        words = [key[0], key[1]]
+
+        #Choose a random value associated with the key
+        while key in chains:
             word = choice(chains[key])
+            #If the value word's last character isn't punctuation, pick a new value 
+            if word[-1] not in punctuation:
+                word = choice(chains[key])
+
             words.append(word)
             key = (key[1], word)
 
-        return " ".join(words)
+        print " ".join(words)
 
 
-class Tweet(SimpleMarkovGenerator):
+
+
+
+
+
+# class Tweet(SimpleMarkovGenerator, StartOnUppercaseMixin, EndsOnPunctuationMixin):
+
+    # while characters < 140
 
 
 
